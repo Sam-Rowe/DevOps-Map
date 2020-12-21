bfcfa4c3843fbb437be56c5aba0280ab36b885a6
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2020-12-21 23:29:00 +0000

    A start

--- devopsmap.py
+++ devopsmap.py
@@ -9,3 +9,36 @@
 
 import json
 import pydot
+
+class Node(object):
+    def __init__(self,name,rest):
+        self.name = name
+        self.label = None
+
+def read_map_json():
+    nodes = {}
+    data = json.load(open("./DevOpsMap.json",'r',encoding='utf-8'))
+
+    for key, value in data.items():
+        nodes[key] = Node(key,value)
+    
+    return nodes
+
+def make_the_map():
+    start = "Automated Testing"
+    graph = pydot.Dot()
+    automatic = pydot.Cluster()
+    # automatic.set_label("automatic")
+    # automatic.set_fontcolour("darkgrey")
+    graph.add_subgraph(automatic)
+
+    nodes = read_map_json()
+    print(nodes)
+
+def main():
+    make_the_map()
+
+
+if __name__ == '__main__':
+    main()
+    
