9af974ad90c80b66bc41faaa64022f7c298290a1
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2020-12-22 18:55:04 +0000

    Builds an SVG, needs Codespace rebuild

--- devopsmap.py
+++ devopsmap.py
@@ -10,30 +10,44 @@
 import json
 import pydot
 
-class Node(object):
+class Practice(object):
     def __init__(self,name,rest):
         self.name = name
         self.label = None
 
+    
+
 def read_map_json():
-    nodes = {}
-    data = json.load(open("./DevOpsMap.json",'r',encoding='utf-8'))
+    practices = {}
+    data = json.load(open("DevOpsMap.json",'r',encoding='utf-8'))
 
     for key, value in data.items():
-        nodes[key] = Node(key,value)
+        practices[key] = Practice(key,value)
     
-    return nodes
+    return practices
 
 def make_the_map():
-    start = "Automated Testing"
+    # start = "Automated Testing"
     graph = pydot.Dot()
-    automatic = pydot.Cluster()
+    # automatic = pydot.Cluster()
     # automatic.set_label("automatic")
     # automatic.set_fontcolour("darkgrey")
-    graph.add_subgraph(automatic)
+    # graph.add_subgraph(automatic)
 
-    nodes = read_map_json()
-    print(nodes)
+    practices = read_map_json()
+
+    for practice in practices:
+        node = pydot.Node(practice)
+        # node.set_label(practice.label)
+        node.set_shape('box')
+        node.set_style('filled,setlinewidth(3.0)')
+        node.set_color('black')
+        node.set_fillcolor('lightgrey')
+        node.set_fontsize('24')
+        graph.add_node(node)
+
+    #print(Practices)
+    graph.write_svg('DevOpsMap.svg')
 
 def main():
     make_the_map()
